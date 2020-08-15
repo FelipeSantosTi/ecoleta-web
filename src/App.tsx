9538ec550c2e4941c4d835d196cadbcf173dc84e
Felipe Santos <felipessantos.ti@gmail.com>
2020-08-15 16:16:12 -0300

    fix: IDE's issues

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,8 @@ import Home from './pages/Home'
 
 function App() {
   return (
-<<<<<<< HEAD
     <Home />
-  )
-=======
-    <h1>Hello, World!</h1>
   );
->>>>>>> dd814959586e8f5ba60848f9ca733d17c434d7bf
 }
 
 export default App;
