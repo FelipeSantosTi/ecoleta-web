06dcd0547dc9ba0d1e100823df8f3f1ee06761f8
Felipe Santos <felipessantos.ti@gmail.com>
2020-08-17 20:34:12 -0300

    chore: Leaflet

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
 import './styles.css'
 import logo from '../../assets/logo.svg'
@@ -48,6 +49,15 @@ const CreatePoint = () => {
             <h2>Endereço</h2>
             <span>Selecione o endereço no mapa</span>
           </legend>
+
+          <Map center={[-1.5398838, -48.6752155]} zoom={15} >
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[-1.5398838, -48.6752155]}/>
+          </Map>
 
           <div className="field-group">
             <div className="field">
